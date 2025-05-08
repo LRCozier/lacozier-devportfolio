df20f46cce362ea0c1f44dea9e011089c35311f0
LRCozier <lrcozier@gmail.com>
2025-05-08 11:06:58 +0100

    created homepage

---
+++ lacportfolio/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Homepage = () => {
+
+  return(
+    <>
+    <section className="portfolio-page" id="portfolio-page-home">
+      <h1>Luke Rudderham-Cozier</h1>
+      <h2>Front End Engineer</h2>
+    </section>
+    </>
+  )
+}
+
+export default Homepage;
