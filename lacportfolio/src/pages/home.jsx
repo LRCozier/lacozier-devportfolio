cc4afe0a860e4dbdda0516be7ab8c78e18485763
LRCozier <lrcozier@gmail.com>
2025-05-08 17:07:58 +0100

    updated homepage contents

--- lacportfolio/src/pages/home.jsx
+++ lacportfolio/src/pages/home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJs, faGitAlt, faNodeJs, faReact, faVuejs, faPhp } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 const Homepage = () => {
 
@@ -10,15 +12,15 @@ const Homepage = () => {
       <h2 className="portfolio-subheading">Front End Engineer</h2>
       <h3 className="portfolio-subheading">Portfolio Under Construction</h3>
       <div className="tech-stack">
-        <FontAwesomeIcon icon="fa-brands fa-html5" />
-        <FontAwesomeIcon icon="fa-brands fa-css" />
-        <FontAwesomeIcon icon="fa-brands fa-js" />
-        <FontAwesomeIcon icon="fa-brands fa-git" />
-        <FontAwesomeIcon icon="fa-brands fa-node-js" />
-        <FontAwesomeIcon icon="fa-brands fa-react" />
-        <FontAwesomeIcon icon="fa-brands fa-vuejs" />
-        <FontAwesomeIcon icon="fa-brands fa-php" />
-        <FontAwesomeIcon icon="fa-solid fa-database" />
+        <FontAwesomeIcon icon={faHtml5} />
+        <FontAwesomeIcon icon={faCss3Alt} />
+        <FontAwesomeIcon icon={faJs} />
+        <FontAwesomeIcon icon={faGitAlt} />
+        <FontAwesomeIcon icon={faNodeJs} />
+        <FontAwesomeIcon icon={faReact} />
+        <FontAwesomeIcon icon={faVuejs} />
+        <FontAwesomeIcon icon={faPhp} />
+        <FontAwesomeIcon icon={faDatabase} />
       </div>
     </section>
     </>
