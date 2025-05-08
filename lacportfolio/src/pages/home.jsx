d63ce2db2a28da4e835beaa3020c7cc9a9015754
LRCozier <lrcozier@gmail.com>
2025-05-08 16:37:54 +0100

    updated homepage

--- lacportfolio/src/pages/home.jsx
+++ lacportfolio/src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Homepage = () => {
 
@@ -7,6 +8,18 @@ const Homepage = () => {
     <section className="portfolio-page" id="portfolio-page-home">
       <h1 className="portfolio-heading">Luke Rudderham-Cozier</h1>
       <h2 className="portfolio-subheading">Front End Engineer</h2>
+      <h3 className="portfolio-subheading">Portfolio Under Construction</h3>
+      <div className="tech-stack">
+        <FontAwesomeIcon icon="fa-brands fa-html5" />
+        <FontAwesomeIcon icon="fa-brands fa-css" />
+        <FontAwesomeIcon icon="fa-brands fa-js" />
+        <FontAwesomeIcon icon="fa-brands fa-git" />
+        <FontAwesomeIcon icon="fa-brands fa-node-js" />
+        <FontAwesomeIcon icon="fa-brands fa-react" />
+        <FontAwesomeIcon icon="fa-brands fa-vuejs" />
+        <FontAwesomeIcon icon="fa-brands fa-php" />
+        <FontAwesomeIcon icon="fa-solid fa-database" />
+      </div>
     </section>
     </>
   )
