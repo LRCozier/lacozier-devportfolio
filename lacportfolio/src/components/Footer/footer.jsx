9d30e171f1bc4a80ef658d17922f143eee97a4c8
LRCozier <lrcozier@gmail.com>
2025-05-08 10:50:40 +0100

    installed font awesome and import icons into footer component

--- lacportfolio/src/components/Footer/footer.jsx
+++ lacportfolio/src/components/Footer/footer.jsx
@@ -1,14 +1,14 @@
 import './Footer.css';
-import {SocialIcon} from 'react-social-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Footer = () => {
   return (
     <footer>
       <p>&copy; 2025 Luke Rudderham-Cozier. All rights reserved.</p>
-      <div id="yogiclaire-footer-socials">
-        <SocialIcon url="https://github.com/LRCozier"/>
-        <SocialIcon url="https://www.linkedin.com/in/luke-rudderham-cozier-30205343/"/>
+      <div className='footer-socials'>
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={Linkedin} />
       </div>
     </footer>
   );
