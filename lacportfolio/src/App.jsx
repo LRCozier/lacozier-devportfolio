a700e63778fb49f33db96f0737a77d14adc4e779
LRCozier <lrcozier@gmail.com>
2025-05-16 08:00:10 +0100

    updated react-router and app.jsx

--- lacportfolio/src/App.jsx
+++ lacportfolio/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
-import Homepage from './pages/home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
+import Homepage from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+
 
 const App = () => {
 
   return (
-    <>
-    <Homepage/>
-    </>
-  )
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
