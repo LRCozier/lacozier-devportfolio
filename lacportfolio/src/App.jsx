341c74b50bc0a370cbdcf797bb218142248a847b
LRCozier <lrcozier@gmail.com>
2025-05-17 18:47:32 +0100

    rearrange app.jsx

--- lacportfolio/src/App.jsx
+++ lacportfolio/src/App.jsx
@@ -11,15 +11,15 @@ const App = () => {
 
   return (
     <BrowserRouter basename="/lacozier-devportfolio">
+     <Navbar />
       <main>
-       <Navbar />
        <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
-    </main>
+     </main>
+     <Footer />
   </BrowserRouter>
   );
 }
