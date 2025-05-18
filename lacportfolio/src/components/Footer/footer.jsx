8127b8236284c69b98a15a4f4c32898e082d8776
LRCozier <lrcozier@gmail.com>
2025-05-18 17:01:43 +0100

    updated footer contents

--- lacportfolio/src/components/Footer/footer.jsx
+++ lacportfolio/src/components/Footer/footer.jsx
@@ -1,4 +1,6 @@
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 
 const Footer = () => {
@@ -6,6 +8,10 @@ const Footer = () => {
     <footer>
       <p>&copy; 2025 Luke Rudderham-Cozier. All rights reserved.</p>
       <div className='footer-socials'>
+        <ul>
+        <a href='https://github.com/LRCozier' target='_blank'><li><FontAwesomeIcon icon={faGithub} /></li></a>
+        <a href='https://www.linkedin.com/in/luke-rudderham-cozier-30205343/' target='_blank'><li><FontAwesomeIcon icon={faLinkedin} /></li></a>
+        </ul>
       </div>
     </footer>
   );
