2e7a91fb5fd1ab733e91dd8bf5fd158d809991df
LRCozier <lrcozier@gmail.com>
2025-05-18 12:13:08 +0100

    updated app.jsx

--- lacportfolio/src/App.jsx
+++ lacportfolio/src/App.jsx
@@ -1,22 +1,30 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TopScroll from './components/Layouts/topscroll';
 import Navbar from './components/Navbar/navbar';
 import Footer from './components/Footer/footer';
 import Homepage from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
+import Projects from './pages/projects';
+import Blog from './pages/blog';
+import NotFound from './pages/notfound';
 
 
 const App = () => {
 
   return (
     <BrowserRouter basename="/lacozier-devportfolio">
+    <TopScroll/>
      <Navbar />
-      <main>
+      <main className='main-content'>
        <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
      </main>
      <Footer />
