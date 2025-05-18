33f7bb31e9d89606f74f69680164aeeb29c20298
LRCozier <lrcozier@gmail.com>
2025-05-18 17:01:52 +0100

    updated projects page

--- lacportfolio/src/pages/projects.jsx
+++ lacportfolio/src/pages/projects.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import './pages.css';
+import './pages.css'
 import Layout from "../components/Layouts/layout";
 import Section from "../components/Layouts/section";
+import ProjectList from "../components/Project Card/projectslist";
 
 const Projects = () => {
 
-  return(
+  return (
     <Layout>
       <Section title="Projects">
-
+        <ProjectList/>
       </Section>
     </Layout>
   )
+
 }
 
 export default Projects;
