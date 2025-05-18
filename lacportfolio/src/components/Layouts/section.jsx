b2ae3c9795e43b4192368a6086d425973876856a
LRCozier <lrcozier@gmail.com>
2025-05-18 11:25:39 +0100

    updated classname in section.jsx

--- lacportfolio/src/components/Layouts/section.jsx
+++ lacportfolio/src/components/Layouts/section.jsx
@@ -5,7 +5,7 @@ const Section = ({title, subtitle, children}) => {
 
   return(
     <>
-    <section className="section">
+    <section className="portfolio-section">
       {title && <h1 className="portfolio-heading">{title}</h1>}
       {subtitle && <h2 className="portfolio-subheading">{subtitle}</h2>}
       {children}
