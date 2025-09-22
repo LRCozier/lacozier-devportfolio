4d7fc4182ae6ea5ca4ff3a7d7f4ee46047aeaf60
LRCozier <lrcozier@gmail.com>
2025-09-22 11:27:55 +0100

    updated contact form

--- lacportfolio/src/components/ContactForm/contactform.jsx
+++ lacportfolio/src/components/ContactForm/contactform.jsx
@@ -32,10 +32,10 @@ const ContactForm = () => {
       setStatus('sending');
       emailjs
         .sendForm(
-          process.env.REACT_APP_EMAILJS_SERVICE_ID, 
-          process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+          import.meta.env.VITE_EMAILJS_SERVICE_ID, 
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
           form.current, 
-          process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
         )
         .then(
           () => {
