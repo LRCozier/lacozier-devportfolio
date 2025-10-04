11bd5c824b529035d5c3fd652d8a0d49fd932293
LRCozier <lrcozier@gmail.com>
2025-10-04 19:09:56 +0100

    updated meta tags and footer info

--- lacportfolio/src/components/Footer/footer.jsx
+++ lacportfolio/src/components/Footer/footer.jsx
@@ -1,17 +1,17 @@
 import './Footer.css';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedin} from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content container">
         <div className="footer-info">
-            <span className="footer-name">Luke Cozier</span>
-            <p>&copy; {new Date().getFullYear()} Luke Cozier. All rights reserved.</p>
+            <span className="footer-name">Luke Rudderham-Cozier</span>
+            <p>&copy; {new Date().getFullYear()} Luke Rudderham-Cozier. All rights reserved.</p>
         </div>
         <div className='footer-socials'>
             <a href='https://github.com/lrcozier' target='_blank' rel="noopener noreferrer" aria-label="GitHub"><FaGithub /></a>
-            <a href='https://www.linkedin.com/in/laurencozier' target='_blank' rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
+            <a href='https://www.linkedin.com/in/luke-rudderham-cozier-30205343/' target='_blank' rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
         </div>
       </div>
     </footer>
